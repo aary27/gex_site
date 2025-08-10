3719610a770b6f4e1cd0f1c5acda7b2b9155de29
Aaryan Aggarwal <aaryan@u.nus.edu>
2025-08-10 12:51:59 +0800

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,45 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>
-      <h1>Welcome to My Website</h1>
-      <nav>
-        <li><Link href="/people">people</Link></li>
-        <li><Link href="/places">places</Link></li>
+    <main
+      style={{
+        backgroundImage: "url('/kert.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        color: "white",
+      }}
+    >
+      {/* Navigation */}
+      <nav
+        style={{
+          display: "flex",
+          gap: "20px",
+          marginTop: "20px",
+          fontSize: "1.2rem",
+        }}
+      >
+        <Link href="/people">About</Link>
+        <Link href="/places">Projects</Link>
       </nav>
+
+      {/* Centered welcome message */}
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1 style={{ fontSize: "3rem", textAlign: "center" }}>
+          Kertasari, Sumbawa
+        </h1>
+      </div>
     </main>
   );
 }
