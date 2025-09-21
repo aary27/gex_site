d0a342729c6d51a23cc97676bc0c8c80e2e5fa6d
Aaryan Aggarwal <aaryan@u.nus.edu>
2025-09-21 15:15:17 +0800

    --

--- src/app/visit/page.tsx
+++ src/app/visit/page.tsx
@@ -30,13 +30,6 @@ export default function Visit() {
                 </Link>
             </nav>
             <h1 className="text-3xl font-bold mb-4">Visit Kertasari</h1>
-
-            <div className="relative w-full max-w-4xl aspect-[16/9] rounded-lg overflow-hidden shadow">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59275.90372254239!2d116.7094301100631!3d-8.710233926864126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcc5cd9c0c40879%3A0x1a0fd924478b6374!2sLabuan%20Kertasari%2C%20Taliwang%2C%20West%20Sumbawa%20Regency%2C%20West%20Nusa%20Tenggara%2C%20Indonesia!5e1!3m2!1sen!2ssg!4v1758437498743!5m2!1sen!2ssg"
-                    width="600" height="450" loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"></iframe>
-            </div>
             <div className="space-y-4 text-gray-800">
                 <h2 className="text-2xl font-semibold">How to Get Here</h2>
                 <p>
@@ -60,6 +53,13 @@ export default function Visit() {
                     hire taxis for the final stretch.
                 </p>
             </div>
+            <div className="relative w-full max-w-4xl aspect-[16/9] rounded-lg overflow-hidden shadow">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59275.90372254239!2d116.7094301100631!3d-8.710233926864126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcc5cd9c0c40879%3A0x1a0fd924478b6374!2sLabuan%20Kertasari%2C%20Taliwang%2C%20West%20Sumbawa%20Regency%2C%20West%20Nusa%20Tenggara%2C%20Indonesia!5e1!3m2!1sen!2ssg!4v1758437498743!5m2!1sen!2ssg"
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
             <section className="mx-auto max-w-6xl">
                 <h2 className="text-2xl font-semibold mb-6 text-center">Places to Stay</h2>
                 <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
