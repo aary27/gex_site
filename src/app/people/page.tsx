d821d849852f3f40f87a47d79dec239234591161
Aaryan Aggarwal <aaryan@u.nus.edu>
2025-09-21 13:47:53 +0800

    --

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 export default function PeoplePage() {
   const peoples = [
-      { href: "/people/posts/seaweed_candy", img: 'seaweed_candy.jpeg', alt: "Seaweed Candy Making" },
-    { href: "/people/posts/lopi", img: "kert.jpg", alt: "Lopi Lopi Racing" },
+      { href: "/people/posts/seaweed_candy", img: 'seaweed_candy.jpeg', alt: "Seaweed Candy Making", label: 'Seaweed Candy Making' },
+    { href: "/people/posts/lopi", img: "kert.jpg", alt: "Lopi Lopi Racing", label: 'Lopi Lopi Racing' },
     // { href: "/people/posts/", img: "/images/post3.jpg", alt: "Post 3" },
   ];
     return (
@@ -22,16 +22,17 @@ export default function PeoplePage() {
           <div className="flex gap-12">
             {peoples.map((blog, idx) => (
                 <Link key={idx} href={blog.href}>
-                  <div
-                      className="w-40 h-40 rounded-full overflow-hidden shadow-lg cursor-pointer transition-transform hover:scale-105">
-                    <Image
-                        src={blog.img}
-                        alt={blog.alt}
-                        width={160}
-                        height={160}
-                        className="object-cover w-full h-full"
-                    />
-                  </div>
+                    <div
+                        className="w-40 h-40 rounded-full overflow-hidden shadow-lg cursor-pointer transition-transform hover:scale-105">
+                        <Image
+                            src={blog.img}
+                            alt={blog.alt}
+                            width={160}
+                            height={160}
+                            className="object-cover w-full h-full"
+                        />
+                    </div>
+                    <p className="text-center text-gray-800 font-medium">{person.label}</p>
                 </Link>
             ))}
           </div>
