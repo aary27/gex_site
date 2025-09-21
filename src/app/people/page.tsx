ab613800c7370fedc50ad17ea8d0a8431fff3638
Aaryan Aggarwal <aaryan@u.nus.edu>
2025-09-21 10:26:49 +0800

    --

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 
 export default function PeoplePage() {
   const peoples = [
-      { href: "/people/posts/seaweed_candy", img: 'url("kert.jpg")', alt: "Seaweed Candy Making" },
-    { href: "/people/posts/lopi", img: "url('kert.jpg')", alt: "Lopi Lopi Racing" },
-    { href: "/people/posts/", img: "/images/post3.jpg", alt: "Post 3" },
+      { href: "/people/posts/seaweed_candy", img: 'url("/kert.jpg")', alt: "Seaweed Candy Making" },
+    { href: "/people/posts/lopi", img: "url('/kert.jpg')", alt: "Lopi Lopi Racing" },
+    // { href: "/people/posts/", img: "/images/post3.jpg", alt: "Post 3" },
   ];
     return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
---
+++ src/app/people/posts/lopi/page.tsx
@@ -0,0 +1,17 @@
+export default function SeaweedPage() {
+  return (
+    <main style={{
+        backgroundImage: "url('/places.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        color: "white",
+      }}>
+      <h1>Places</h1>
+      <p>**WIP** showcase natural attractions and things to do/how to get to the vilage</p>
+    </main>
+  );
+}
