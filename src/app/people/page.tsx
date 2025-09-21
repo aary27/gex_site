327840eb2791e521608c13339de45f86eeeebc91
Aaryan Aggarwal <aaryan@u.nus.edu>
2025-09-21 10:15:59 +0800

    --

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -4,23 +4,27 @@ import Image from 'next/image';
 
 export default function PeoplePage() {
   const peoples = [
-      { href: "/blogposts/post1", img: "/images/post1.jpg", alt: "Post 1" },
+      { href: "/post1", img: "/images/post1.jpg", alt: "Post 1" },
     { href: "/blogposts/post2", img: "/images/post2.jpg", alt: "Post 2" },
     { href: "/blogposts/post3", img: "/images/post3.jpg", alt: "Post 3" },
   ];
     return (
-    <main style={{
-        backgroundImage: "url('/people.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        color: "white",
-      }}>
-      <h1>People</h1>
-      <p>**WIP** Enter info for people/culture including seagrass candy/lopi lopi</p>
-    </main>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="flex gap-12">
+        {peoples.map((blog, idx) => (
+          <Link key={idx} href={blog.href}>
+            <div className="w-40 h-40 rounded-full overflow-hidden shadow-lg cursor-pointer transition-transform hover:scale-105">
+              <Image
+                src={blog.img}
+                alt={blog.alt}
+                width={160}
+                height={160}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
